02ef7261df9b4f257d5f0a4cb552883fc40dde2c
Your Name <ghalibahmed5000@gmail.com>
2019-09-25 20:54:42 +0500

    added the final requirements

--- PC_Camera/PC_Camera/MainWindow.xaml.cs
+++ PC_Camera/PC_Camera/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using Camera_NET;
 using System.Timers;
 using System.Threading;
 using System.Diagnostics;
+using System.Drawing.Imaging;
 
 namespace PC_Camera
 {
@@ -26,15 +27,15 @@ namespace PC_Camera
     /// </summary>
     public partial class MainWindow : Window
     {
+        int min = 5;
+        string current_time = "";
         public MainWindow()
         {
-            int min = 1;
-            DateTime dt = new DateTime();
+            string location = @"D:\New folder12\PC Camera\";
+
             InitializeComponent();
-            CameraControl cam = new CameraControl();
             CameraChoice _CameraChoice = new CameraChoice();
-            //CameraControl cam = new CameraControl();
-            Camera c = new Camera();
+            CameraControl cam = new CameraControl();
             _CameraChoice.UpdateDeviceList();
             var moniker = _CameraChoice.Devices[0].Mon;
             ResolutionList resolutions = Camera.GetResolutionList(moniker);
@@ -46,37 +47,70 @@ namespace PC_Camera
             }
             Res.Text += "Starting Camera" + "\n";
 
-           var timer = new System.Timers.Timer();
-            timer.Interval = 5000;
+            var timer = new System.Timers.Timer();
+            timer.Interval = 1000 * min;
             timer.Elapsed += ontimed;
             timer.Start();
-             void ontimed(object sender, System.Timers.ElapsedEventArgs e)
-                {
-                test();
+            void ontimed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                current_time = DateTime.Now.ToString("dddd dd MMMM yyyy hh.mm.ss tt");
+
+                test(current_time);
+                pc_img(current_time);
+                time_out.Text = "image captured at time " + current_time+"\n";
+                timer.Interval = (1000 * 60) * min;
             }
-            void test()
+            void pc_img(string time)
+            {
+
+                var image = ScreenCapture.CaptureDesktop();
+                image.Save(location + @"PC Images\" + time + " PC.jpg", ImageFormat.Jpeg);
+
+                {
+                    /*
+                    System.Drawing.Rectangle bounds = Screen.GetBounds(System.Drawing.Point.Empty);
+                    using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                    {
+                        using (Graphics g = Graphics.FromImage(bitmap))
+                        {
+                            g.CopyFromScreen(System.Drawing.Point.Empty, System.Drawing.Point.Empty, bounds.Size);
+                        }
+                        bitmap.Save("D:\\test.jpg", ImageFormat.Jpeg);
+                    }
+                    */
+                }
+            }
+            void test(string time)
             {
                 this.Dispatcher.Invoke(() =>
                 {
-                    try
+                    //   try
                     {
-                        time_out.Text += Convert.ToString(DateTime.Now) + "\n";
+
+                        image_taker(time);
                     }
-                    catch (Exception ex)
+                    //  catch (Exception ex)
                     {
-                        time_out.Text = ex.ToString();
+                        //     time_out.Text = ex.ToString();
                     }
                 });
 
             }
-            void image_taker()
+            void image_taker(string time)
             {
+
                 //image taker
+
                 cam.SetCamera(moniker, resolutions[0]);
-                cam.SnapshotSourceImage().Save("D:\\out.png");
-                cam.Dispose();
+                cam.SnapshotSourceImage().Save(location + @"User Images\" + time + " User.png");
+                // cam.Dispose();
             }
 
+        }
+
+        private void Set_mins_Click(object sender, RoutedEventArgs e)
+        {
+            min = Convert.ToInt32(mins_num.Text);
         }
     }
 }
